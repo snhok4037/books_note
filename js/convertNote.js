ad1aaa70fd3f80d96e1b8384d44045f4b577cf6e
shkwon <soonho36@gmail.com>
2021-05-21 21:01:45 +0900

    리디북스 페이지 html에서 책 제목 추출

--- js/convertNote.js
+++ js/convertNote.js
@@ -1,4 +1,18 @@
-const convertNoteBTN = document.querySelector("#convertNote");
+const convertNoteBTN = document.querySelector("#convertNote")
+
 convertNoteBTN.addEventListener("click", function(){
-	alert("변경");
+	const beforeChangeNoteValue = document.getElementById("beforeChangeNote").value;
+	const beforeChangeNoteBox = document.createElement('span');
+	beforeChangeNoteBox.className += "hidden";
+	beforeChangeNoteBox.innerHTML = beforeChangeNoteValue;
+	document.body.append(beforeChangeNoteBox);
+
+	var bookName = getBookName(document.querySelector("span #app"));
+
+	document.getElementById("afterChangeNote").value = "# "+bookName;
 }, false);
+
+
+function getBookName(noteHtml){
+	return document.querySelector("#page_reading_note_detail > article > div > div > h3 > a").innerText;
+}
