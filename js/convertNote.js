41516dc444e8e907e1ed0aa700523b3e7b3a4cf4
shkwon <shkwon@gmail.com>
2021-05-23 20:19:09 +0900

    책 제목 / 지은이(와 번역자) 추출 완료 HTML 요소가 지정되어있어 선택자 사용하여 선택

--- js/convertNote.js
+++ js/convertNote.js
@@ -1,18 +1,43 @@
-const convertNoteBTN = document.querySelector("#convertNote")
+const convertNoteBTN = document.querySelector("#convertNote");
+const noteContentSelector = "span #app #page_reading_note_detail > article > div.css-5f4m9y > div.css-4wuuch";
 
 convertNoteBTN.addEventListener("click", function(){
 	const beforeChangeNoteValue = document.getElementById("beforeChangeNote").value;
 	const beforeChangeNoteBox = document.createElement('span');
-	beforeChangeNoteBox.className += "hidden";
 	beforeChangeNoteBox.innerHTML = beforeChangeNoteValue;
 	document.body.append(beforeChangeNoteBox);
 
-	var bookName = getBookName(document.querySelector("span #app"));
+	var bookName = getBookName(),
+		bookWriter = getBookWriter();
 
-	document.getElementById("afterChangeNote").value = "# "+bookName;
+	var noteHeader = bookName+" - "+bookWriter;
+
+	var afterChangeNote = noteHeader;
+	document.getElementById("afterChangeNote").value = afterChangeNote;
 }, false);
 
-
-function getBookName(noteHtml){
-	return document.querySelector("#page_reading_note_detail > article > div > div > h3 > a").innerText;
+function getNoteData(ele){
+	
+	console.log(noteContentSelector + " > " +ele + " --- ");
+	console.log(document.querySelectorAll(noteContentSelector + " > " +ele));
+	return document.querySelector(noteContentSelector + " > " +ele).innerText;
 }
+
+/* 책 제목 */
+function getBookName(){
+	const bookNameSelector = "h3.css-bm6nz6 > a";
+	return getNoteData(bookNameSelector);
+}
+/* 책 저자 */
+function getBookWriter(){
+	const bookWriterSelector = "div.css-0 > span";
+
+	const writerEle = document.querySelectorAll(noteContentSelector+" > "+bookWriterSelector),
+		writerCnt = writerEle.length;
+	if (writerCnt !== 1) {
+		return writerEle[0]+" 저 / "+writerEle[1]+" 역 ";
+	} else {
+		return getNoteData(bookWriterSelector)+" 저 ";
+	}
+}
+/* 책 번역자 */
